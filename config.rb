e4c1d0ddcea3812419de9c327faadd1809e3a006
Carolann Bonner <carolanncan@gmail.com>
2014-12-27 21:44:58 -0500

    Initial commit

---
+++ config.rb
@@ -0,0 +1,26 @@
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+  set :http_prefix, "/Content/images/"
+end
+
+configure :development do
+  activate :livereload
+end
+#
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
